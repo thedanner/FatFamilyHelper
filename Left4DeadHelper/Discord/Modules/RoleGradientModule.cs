3cd4737d5088970e1ebec50694e9450fd0566435
Dan Mangiarelli <dan.mangiarelli@gmail.com>
2020-12-31 19:28:34 -0500

    Minor tweaks to RoleGradientModule.
    #5

--- Left4DeadHelper/Discord/Modules/RoleGradientModule.cs
+++ Left4DeadHelper/Discord/Modules/RoleGradientModule.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.Commands;
 using Left4DeadHelper.Helpers;
 using Left4DeadHelper.Models;
+using Left4DeadHelper.Wrappers.DiscordNet;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
@@ -14,13 +15,13 @@ namespace Left4DeadHelper.Discord.Modules
         private const string Command = "roles";
 
         public ILogger<PickMapModule> Logger { get; }
-        public IServiceProvider ServiceProvider { get; }
+        public IDiscordSocketClientWrapper Client { get; }
         public Settings Settings { get; }
 
-        public RoleGradientModule(ILogger<PickMapModule> logger, IServiceProvider serviceProvider, Settings settings)
+        public RoleGradientModule(ILogger<PickMapModule> logger, IDiscordSocketClientWrapper client, Settings settings)
         {
             Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            ServiceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            Client = client ?? throw new ArgumentNullException(nameof(client));
             Settings = settings ?? throw new ArgumentNullException(nameof(settings));
         }
 
@@ -28,7 +29,9 @@ namespace Left4DeadHelper.Discord.Modules
         [Summary("Sets role colors!")]
         public async Task HandleCommandAsync(params string[] args)
         {
-
+            // TODO figure out how to get guild roles in the same order as the UI shows.
+            // TODO run gradient calculation.
+            // TODO actually set role colors!
         }
     }
 }
