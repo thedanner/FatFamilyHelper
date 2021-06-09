a2d46ad0fc500d1acb8cd574df561c1585a7e613
Dan Mangiarelli <dan.mangiarelli@gmail.com>
2021-06-09 17:56:14 -0400

    WIP - spray tools in l4d bot

---
+++ Left4DeadHelper.Sprays/SprayTools.cs
@@ -0,0 +1,102 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Tga;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Left4DeadHelper.Sprays
+{
+    public class SprayTools
+    {
+        private const int MaxWidth = 1024;
+        private const int MaxHeight = 1024;
+
+        // TODO: support animated sprays
+        // TODO: optionally support VDF format?
+        // TODO: support creating near/far sprays (requires VDF format support)
+
+        public async Task<Stream> ConvertAsync(Stream inputStream, CancellationToken cancellationToken)
+        {
+            if (inputStream == null) throw new ArgumentNullException(nameof(inputStream));
+
+            var memoryStream = new MemoryStream();
+            await inputStream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+
+            var inputFormat = Image.DetectFormat(memoryStream);
+            memoryStream.Position = 0;
+            var inputMimeTypes = inputFormat.MimeTypes.ToList();
+            var allowedMimeTypes = new[] { "image/jpeg", "image/png" };
+
+            if (!allowedMimeTypes.Intersect(inputMimeTypes).Any())
+            {
+                throw new Exception($"None of the following MIME types are currently supported: \"{string.Join("\", \"", inputMimeTypes)}\".");
+            }
+
+            using var image = await Image.LoadAsync(Configuration.Default, memoryStream, cancellationToken);
+
+            // If too big, scale down. Could be done with a transform below but this is simpler.
+            if (image.Width > MaxWidth || image.Height > MaxHeight)
+            {
+                double scaleFactor;
+
+                if (image.Width >= image.Height)
+                {
+                    scaleFactor = 1.0 * MaxWidth / image.Width;
+                }
+                else
+                {
+                    scaleFactor = 1.0 * MaxHeight / image.Height;
+                }
+
+                image.Mutate(x => x.Resize(
+                    (int)Math.Floor(image.Width * scaleFactor),
+                    (int)Math.Floor(image.Height * scaleFactor)
+                ));
+            }
+
+            // 
+            if (image.Width != MaxWidth || image.Height != MaxHeight)
+            {
+                var resizeOptions = new ResizeOptions
+                {
+                    Size = new Size
+                    {
+                        Width = MaxWidth,
+                        Height = MaxHeight
+                    },
+                    Mode = ResizeMode.Manual,
+                    Position = AnchorPositionMode.TopLeft,
+                    TargetRectangle = new Rectangle
+                    {
+                        X = (MaxWidth - image.Width) / 2,
+                        Y = (MaxHeight - image.Height) / 2,
+                        Width = image.Width,
+                        Height = image.Height
+                    },
+                    Compand = false,
+                };
+
+                image.Mutate(x => x
+                    .Resize(resizeOptions)
+                    .BackgroundColor(Color.Transparent));
+            }
+
+            var encoder = new TgaEncoder
+            {
+                BitsPerPixel = TgaBitsPerPixel.Pixel32,
+                Compression = TgaCompression.None,
+            };
+
+            var outputStream = new MemoryStream();
+            await image.SaveAsync(outputStream, encoder, cancellationToken);
+
+            outputStream.Position = 0;
+
+            return outputStream;
+        }
+    }
+}
---
+++ Left4DeadHelper/Discord/Modules/SprayModule.cs
@@ -0,0 +1,106 @@
+﻿using Discord;
+using Discord.Commands;
+using Left4DeadHelper.Helpers;
+using Left4DeadHelper.Models;
+using Left4DeadHelper.Sprays;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Left4DeadHelper.Discord.Modules
+{
+    [Group(Constants.GroupSprayMe)]
+    public class SprayModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly ILogger<PickMapModule> _logger;
+        private readonly Settings _settings;
+
+        public SprayModule(ILogger<PickMapModule> logger, Settings settings)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _settings = settings ?? throw new ArgumentNullException(nameof(settings));
+        }
+
+        [Command]
+        [Summary("Converts an image into a Source engine-compatible spray")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task HandleCommandAsync(string? url = null)
+        {
+            var client = new WebClient();
+            var replyToMessageRef = new MessageReference(Context.Message.Id, Context.Channel.Id, Context.Guild.Id);
+
+            _logger.LogInformation("Triggered by message with ID {0}.", Context.Message.Id);
+
+            Uri? imageUri = null;
+            if (url != null && Uri.TryCreate(url, UriKind.Absolute, out imageUri))
+            {
+                // All good here.
+                _logger.LogInformation("Got URL as first token in message.");
+            }
+            else if (Context.Message.Attachments.Any())
+            {
+                _logger.LogInformation("Got URL as attachment message.");
+                Uri.TryCreate(Context.Message.Attachments.First().Url, UriKind.Absolute, out imageUri);
+            }
+            else if (Context.Message.ReferencedMessage != null)
+            {
+                _logger.LogInformation("Checking referenced message.");
+
+                var originalMessage = Context.Message.ReferencedMessage;
+                var firstToken = originalMessage.Content?.Trim();
+
+                if (!string.IsNullOrEmpty(firstToken))
+                {
+                    if (firstToken.Contains(' '))
+                    {
+                        firstToken = firstToken.Substring(0, firstToken.IndexOf(' '));
+                    }
+
+                    if (firstToken != null && Uri.TryCreate(firstToken, UriKind.Absolute, out imageUri))
+                    {
+                        _logger.LogInformation("Got URL as first token in referenced message.");
+                        // All good here.
+                    }
+                }
+                else if (originalMessage.Attachments.Any())
+                {
+                    _logger.LogInformation("Got URL as first attachment in referenced message.");
+                    Uri.TryCreate(originalMessage.Attachments.First().Url, UriKind.Absolute, out imageUri);
+                }
+            }
+
+            // No source found
+            if (imageUri == null) return;
+
+            try
+            {
+                var tempMessage = await ReplyAsync("Working on it...", messageReference: replyToMessageRef);
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+
+                using var sourceStream = await client.OpenReadTaskAsync(imageUri);
+
+                var sprayTools = new SprayTools();
+                var convertedStream = await sprayTools.ConvertAsync(sourceStream, CancellationToken.None);
+
+                var filePart = imageUri.LocalPath;
+                if (filePart.Contains('/')) filePart = filePart.Substring(filePart.LastIndexOf('/') + 1);
+                filePart = Path.ChangeExtension(filePart, ".tga");
+
+                await Context.Channel.SendFileAsync(convertedStream, filePart, "Here ya go!", messageReference: replyToMessageRef);
+                await Task.Delay(TimeSpan.FromMilliseconds(500));
+
+                await tempMessage.DeleteAsync();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Got an error converting image to a spray :(");
+                throw;
+            }
+        }
+    }
+}
