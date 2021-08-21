05716ad203a81316d4d898b6f71b76e96c930fe9
thedanner <66450828+thedanner@users.noreply.github.com>
2021-08-20 20:41:37 -0400

    Delete unused class.

--- Left4DeadHelper/Discord/Tasks/DeleteExpiredBorderlandsCodesSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Left4DeadHelper.Discord.Modules
-{
-    public class DeleteExpiredBorderlandsCodesSettings
-    {
-        public ulong GuildId { get; set; }
-        public ulong ChannelId { get; set; }
-    }
-}
